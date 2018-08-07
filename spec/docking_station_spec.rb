61a78d41e8a43a80558431662635b237afd1d0f0
Samir Soormally <samirsoormally@gmail.com>
2018-08-07 16:44:51 +0100

    added rspec test for release_bike method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,7 @@
 require "docking_station"
 
 describe DockingStation do
-
+  it "should give response" do
+    expect(docking_station = DockingStation.new, docking_station.release_bike).to eq "bike released"
+  end
 end
