f503202bc24b28e9a0d514d7a7bc958264a609d3
jdm79 <james.malvern@gmail.com>
2018-08-08 16:10:44 +0100

    delete comment

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-# You have to alter the DockingStation class so release_bike returns a new instance of the Bike class.
 require './lib/bike'
-=======
-require_relative 'bike'
->>>>>>> 01fb34badab4b384c18419671a6ba6a41ce4b60c
 
 class DockingStation
   def release_bike
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -9,8 +9,4 @@ describe DockingStation do
     bike = subject.release_bike
     expect(bike).to be_working
   end
-<<<<<<< HEAD
 end
-=======
-end
->>>>>>> 01fb34badab4b384c18419671a6ba6a41ce4b60c
