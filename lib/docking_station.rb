a0418b3c17b92a52f6675afd15903de65f2f3163
jdm79 <james.malvern@gmail.com>
2018-08-08 15:36:38 +0100

    fix typo

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,5 +1,8 @@
+# You have to alter the DockingStation class so release_bike returns a new instance of the Bike class.
+require './lib/bike'
+
 class DockingStation
   def release_bike
-    
+    Bike.new
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,5 +4,9 @@ describe DockingStation do
   it "releases a bike" do
     expect(subject).to respond_to :release_bike
   end
-end
 
+  it "releases working bikes" do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
+end
